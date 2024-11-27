f95586f432d0b62abd6a008eb2c49a06c9486042
RPengo <34013727+RPengo@users.noreply.github.com>
2024-11-27 12:40:14 -0400

    Update many.py

--- many.py
+++ many.py
@@ -4,36 +4,11 @@ from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import requests
 import zipfile
-from datetime import datetime, timedelta
+from datetime import datetime
 import json
 import os
 
 app = Flask(__name__)
-
-def baixar_e_processar_dados(ano, mes):
-    try:
-        url = f'https://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_{ano}{mes}.zip'
-        download = requests.get(url)
-
-        if download.status_code == 200:
-            with open(f"inf_diario_fi_{ano}{mes}.zip", "wb") as arquivo_cvm:
-                arquivo_cvm.write(download.content)
-
-            arquivo_zip = zipfile.ZipFile(f"inf_diario_fi_{ano}{mes}.zip")
-            dados_fundos = pd.read_csv(arquivo_zip.open(arquivo_zip.namelist()[0]), sep=";", encoding='ISO-8859-1')
-            
-            dados_cadastro = pd.read_csv('https://dados.cvm.gov.br/dados/FI/CAD/DADOS/cad_fi.csv', sep=";", encoding='ISO-8859-1')
-            dados_cadastro = dados_cadastro[['CNPJ_FUNDO', 'DENOM_SOCIAL']].drop_duplicates()
-
-            base_final = pd.merge(dados_fundos, dados_cadastro, how="left", left_on="CNPJ_FUNDO", right_on="CNPJ_FUNDO")
-            base_final = base_final[['CNPJ_FUNDO', 'DENOM_SOCIAL', 'DT_COMPTC', 'VL_QUOTA', 'VL_PATRIM_LIQ', 'NR_COTST']]
-
-            return base_final
-        else:
-            return pd.DataFrame()  # Retorna DataFrame vazio se não conseguir baixar o arquivo
-    except Exception as e:
-        print(f"Erro ao baixar ou processar os dados: {str(e)}")
-        return pd.DataFrame()
 
 @app.route("/")
 def update_spreadsheet():
@@ -64,54 +39,57 @@ def update_spreadsheet():
             # Criar DataFrame vazio se a planilha estiver vazia
             existing_df = pd.DataFrame(columns=['Nome do Fundo', 'CNPJ', 'Valor da Cota', 'Data da Cota'])
 
-        # Baixar e processar os dados dos fundos do mês atual
+        # Baixar e processar os dados dos fundos
         hoje = datetime.today()
-        ano_atual = hoje.strftime('%Y')
-        mes_atual = hoje.strftime('%m')
+        ano = hoje.strftime('%Y')
+        mes = hoje.strftime('%m')
+        url = f'https://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_{ano}{mes}.zip'
 
-        base_atual = baixar_e_processar_dados(ano_atual, mes_atual)
+        download = requests.get(url)
 
-        # Verificar se todos os fundos específicos têm dados no arquivo atual
+        with open(f"inf_diario_fi_{ano}{mes}.zip", "wb") as arquivo_cvm:
+            arquivo_cvm.write(download.content)
+
+        arquivo_zip = zipfile.ZipFile(f"inf_diario_fi_{ano}{mes}.zip")
+        dados_fundos = pd.read_csv(arquivo_zip.open(arquivo_zip.namelist()[0]), sep=";", encoding='ISO-8859-1')
+
+        dados_cadastro = pd.read_csv('https://dados.cvm.gov.br/dados/FI/CAD/DADOS/cad_fi.csv', sep=";", encoding='ISO-8859-1')
+        dados_cadastro = dados_cadastro[['CNPJ_FUNDO', 'DENOM_SOCIAL']].drop_duplicates()
+
+        base_final = pd.merge(dados_fundos, dados_cadastro, how="left", left_on="CNPJ_FUNDO", right_on="CNPJ_FUNDO")
+        base_final = base_final[['CNPJ_FUNDO', 'DENOM_SOCIAL', 'DT_COMPTC', 'VL_QUOTA', 'VL_PATRIM_LIQ', 'NR_COTST']]
+
         fundos_especificos = [
-            "26.673.556/0001-32",
-            "10.347.493/0001-94",
-            "23.272.391/0001-07", 
-            "08.830.947/0001-31",
-            "37.910.132/0001-60",
-            "32.990.051/0001-02", 
-            "30.566.221/0001-92",
-            "34.583.819/0001-40",
-            "34.780.531/0001-66", 
-            "45.278.833/0001-57",
-            "32.893.503/0001-20",
-            "35.471.498/0001-55", 
-            "30.509.221/0001-50",
-            "12.154.412/0001-65",
-            "39.959.025/0001-52", 
-            "32.892.827/0001-43",
-            "39.586.835/0001-00",
-            "33.520.968/0001-06", 
-            "22.918.359/0001-85",
-            "42.794.534/0001-87",
-            "44.211.851/0001-59", 
-            "42.922.205/0001-74",
-            "35.956.641/0001-07",
-            "37.053.502/0001-90",
-            "10.843.445/0001-97",
+        "26.673.556/0001-32",
+        "10.347.493/0001-94",
+        "23.272.391/0001-07",
+        "08.830.947/0001-31",
+        "37.910.132/0001-60",
+        "32.990.051/0001-02",
+        "30.566.221/0001-92",
+        "34.583.819/0001-40",
+        "34.780.531/0001-66",
+        "45.278.833/0001-57",
+        "32.893.503/0001-20",
+        "35.471.498/0001-55",
+        "30.509.221/0001-50",
+        "12.154.412/0001-65",
+        "39.959.025/0001-52",
+        "32.892.827/0001-43",
+        "39.586.835/0001-00",
+        "33.520.968/0001-06",
+        "22.918.359/0001-85",
+        "42.794.534/0001-87",
+        "44.211.851/0001-59",
+        "42.922.205/0001-74",
+        "35.956.641/0001-07",
+        "37.053.502/0001-90",
         ]
 
         dados_planilha = []
-        for cnpj in fundos_especificos:
-            fundo = base_atual[base_atual['CNPJ_FUNDO'] == cnpj]
 
-            # Se o fundo não estiver no arquivo atual, buscar dados do mês anterior
-            if fundo.empty:
-                # Calcular o mês anterior
-                mes_anterior = (hoje - timedelta(days=30)).strftime('%m')
-                ano_anterior = (hoje - timedelta(days=30)).strftime('%Y')
-                
-                base_anterior = baixar_e_processar_dados(ano_anterior, mes_anterior)
-                fundo = base_anterior[base_anterior['CNPJ_FUNDO'] == cnpj]
+        for cnpj in fundos_especificos:
+            fundo = base_final[base_final['CNPJ_FUNDO'] == cnpj]
 
             if not fundo.empty:
                 ultima_data = fundo['DT_COMPTC'].max()
@@ -161,5 +139,5 @@ def update_spreadsheet():
 # Adicione esta parte ao final do código para definir a porta
 if __name__ == "__main__":
     # O Flask deve escutar na porta definida pela variável de ambiente PORT
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
